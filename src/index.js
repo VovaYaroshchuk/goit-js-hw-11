561f343f37d5c67b1de4fed28f70a5a2530fca13
Vova Yaroshchuk <voyaroshchuk@gmail.com>
2022-06-23 17:00:04 +0300

    fix all

--- src/index.js
+++ src/index.js
@@ -1,57 +1,113 @@
-const inputEL = document.querySelector('input[name="searchQuery"]');
-const searchBtn = document.querySelector('button[type="submit"]');
+import Notiflix from 'notiflix';
+const axios = require('axios').default;
+import SimpleLightbox from "simplelightbox";
+
+const searchForm = document.querySelector('#search-form')
 const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+const API_KEY ='28211530-9aacd352b3f11c956fdc5a9f9';
+let simpleLightBox
+let query = ''
+let page = 1
+
+const perPage = 40
+axios.defaults.baseURL = 'https://pixabay.com/api/'
 
 
-
-
-searchBtn.addEventListener('click', searchImages);
-
-function searchImages (e) {
-  e.preventDefault();
-
-  const searchQuery = inputEL.value;
-const BASIC_URL = 'https://pixabay.com/api/'
-
-const searchParams = new URLSearchParams({
-    key: "28211530-9aacd352b3f11c956fdc5a9f9",
-    q: searchQuery,
-    image_type: "photo",
-    orientation: "horizontal",
-    safesearch: "true",
-  });
-
-const url = `${BASIC_URL}?${searchParams}`;
- function fetchImages(url) {
-    return fetch(url)
-        .then(response => response.json())
-        .then(data => renderImagesList(data.hits))
-        .catch(error => console.log(error));
+async function fetchImages(query, page, perPage) {
+  const response = await axios.get(
+    `?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
+  )
+  return response
 }
-fetchImages(url);
+
+function renderGallery(images) {
+  const markup = images
+    .map(image => {
+      const { id, largeImageURL, webformatURL, tags, likes, views, comments, downloads } = image;
+      return `
+        <a class="gallery__link" href="${largeImageURL}">
+          <div class="gallery-item" id="${id}">
+            <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item"><b>Likes</b>${likes}</p>
+              <p class="info-item"><b>Views</b>${views}</p>
+              <p class="info-item"><b>Comments</b>${comments}</p>
+              <p class="info-item"><b>Downloads</b>${downloads}</p>
+            </div>
+          </div>
+        </a>
+      `;
+    })
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
 }
-  
-  function renderImagesList(images) {
-    const markup = images
-      .map((image) => {
-        return `<div class="photo-card">
-        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-        <div class="info">
-          <p class="info-item">
-            <b>Likes ${image.likes}</b>
-          </p>
-          <p class="info-item">
-            <b>Views ${image.views}</b>
-          </p>
-          <p class="info-item">
-            <b>Comments ${image.comments}</b>
-          </p>
-          <p class="info-item">
-            <b>Downloads ${image.downloads}</b>
-          </p>
-        </div>
-      </div>`;
-      })
-      .join("");
-      gallery.innerHTML = markup;
+
+searchForm.addEventListener('submit', onSearchForm)
+loadMoreBtn.addEventListener('click', onLoadMoreBtn)
+
+
+function onSearchForm(e) {
+  e.preventDefault()
+  page = 1
+  query = e.currentTarget.searchQuery.value.trim()
+  gallery.innerHTML = ''
+  loadMoreBtn.classList.add('is-hidden')
+
+  if (query === '') {
+    alertNoEmptySearch()
+    return
   }
+
+  fetchImages(query, page, perPage)
+    .then(({ data }) => {
+      if (data.totalHits === 0) {
+        alertNoImagesFound()
+      } else {
+        renderGallery(data.hits)
+        simpleLightBox = new SimpleLightbox('.gallery a').refresh()
+        alertImagesFound(data)
+
+        if (data.totalHits > perPage) {
+          loadMoreBtn.classList.remove('is-hidden')
+        }
+      }
+    })
+    .catch(error => console.log(error))
+}
+
+function onLoadMoreBtn() {
+  page += 1
+  simpleLightBox.destroy()
+
+  fetchImages(query, page, perPage)
+    .then(({ data }) => {
+      renderGallery(data.hits)
+      simpleLightBox = new SimpleLightbox('.gallery a').refresh()
+
+      const totalPages = Math.ceil(data.totalHits / perPage)
+
+      if (page > totalPages) {
+        loadMoreBtn.classList.add('is-hidden')
+        alertEndOfSearch()
+      }
+    })
+    .catch(error => console.log(error))
+}
+
+function alertImagesFound(data) {
+  Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
+}
+
+function alertNoEmptySearch() {
+  Notiflix.Notify.failure('The search string cannot be empty. Please specify your search query.')
+}
+
+function alertNoImagesFound() {
+  Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+}
+
+function alertEndOfSearch() {
+  Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.")
+}
